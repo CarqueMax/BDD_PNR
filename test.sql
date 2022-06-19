5c9509dc150bf814747d6e034894e6c3e92772dc
MaMax <40518331+CarqueMax@users.noreply.github.com>
2022-06-19 15:13:29 +0200

    Update and rename test to test.sql

--- test.sql
+++ test.sql
@@ -4,8 +4,9 @@ CREATE TABLE Utilisateur(
     nom VARCHAR(50),
     mdpUtilisateur VARCHAR(50),
     permission INTEGER,
+    prenom VARCHAR(50),
 
     CONSTRAINT pk_Utilisateur PRIMARY KEY (nom)
 );
 
-INSERT INTO Utilisateur VALUES ('nomtest','test',0);
+INSERT INTO Utilisateur VALUES ('nomtest','test',0, 'Miel Pops');
