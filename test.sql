c976550388090a4fa7b7bce5b22bd204c580c3cf
MaMax <40518331+CarqueMax@users.noreply.github.com>
2022-06-20 17:21:46 +0200

    Update test.sql

--- test.sql
+++ test.sql
@@ -1,12 +1,22 @@
 DROP TABLE Utilisateur;
 
 CREATE TABLE Utilisateur(
+    pseudonyme VARCHAR(255),
     nom VARCHAR(50),
+    prenom VARCHAR(50),
     mdpUtilisateur VARCHAR(50),
     permission INTEGER,
-    prenom VARCHAR(50),
 
-    CONSTRAINT pk_Utilisateur PRIMARY KEY (nom)
+    CONSTRAINT pk_Utilisateur PRIMARY KEY (pseudonyme)
 );
 
-INSERT INTO Utilisateur VALUES ('nomtest','test',0, 'Miel Pops');
+INSERT INTO Utilisateur VALUES ('AS', 'AlphardHydrae', 'Sophie', 'hydrae', 1);
+INSERT INTO Utilisateur VALUES ('basicfat', 'annaggn', 'anna', 'salut', 1);
+INSERT INTO Utilisateur VALUES ('everyone', 'arwen', 'arwen', 'arwen', 0);
+INSERT INTO Utilisateur VALUES ('ok', 'jean', 'Anna', 'salut2', 0);
+INSERT INTO Utilisateur VALUES ('test', 'jetest', 'salut', ' coucou', 0);
+INSERT INTO Utilisateur VALUES ('chelou', 'louis', 'pierre', 'non', 0);
+INSERT INTO Utilisateur VALUES ('chelou2', 'marc', 'zczj', 'non', 0);
+INSERT INTO Utilisateur VALUES ('ME', 'Morvan', 'Enzo', 'helloworld', 0);
+INSERT INTO Utilisateur VALUES ('wouaw', 're', 'aucunsens', 'jesuistropnulle', 1);
+INSERT INTO Utilisateur VALUES ('SM', 'Safe', 'Maxime', 'salut', 1);
