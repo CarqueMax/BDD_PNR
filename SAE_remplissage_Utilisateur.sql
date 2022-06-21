8e5c483913f29535c9e9da583cec64496dc808b6
MaMax <40518331+CarqueMax@users.noreply.github.com>
2022-06-21 22:49:10 +0200

    Update SAE_remplissage_Utilisateur.sql

--- SAE_remplissage_Utilisateur.sql
+++ SAE_remplissage_Utilisateur.sql
@@ -1,4 +1,4 @@
 DELETE FROM Utilisateur;
 
-INSERT INTO Utilisateur VALUES ('user', 'nom_user', 'prenom_user', 'mdp_user', 0);
-INSERT INTO Utilisateur VALUES ('admin', 'nom_admin', 'prenom_admin', 'mdp_admin', 1);
+INSERT INTO Utilisateur VALUES ('user', 'nom_user', 'prenom_user', '474d6d33494c4f8017d7fa7da15c198e', 0);
+INSERT INTO Utilisateur VALUES ('admin', 'nom_admin', 'prenom_admin', '5690f6355932258334005760778de9f5', 1);
